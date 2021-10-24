9576e5e4330ab4134ac5bb4a546c536bb41071cc
rsh-12 <grinvichforum12@mail.ru>
2021-10-24 10:49:05 +0500

    Add migration file

---
+++ migrations/1635051632076_add-companies-table.js
@@ -0,0 +1,24 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+    pgm.sql(`
+        CREATE TABLE companies
+        (
+            id          SERIAL PRIMARY KEY,
+            name        VARCHAR(30) NOT NULL,
+            description VARCHAR(120),
+            created_at  TIMESTAMP DEFAULT current_timestamp,
+            upated_at   TIMESTAMP DEFAULT current_timestamp
+        );
+
+        COMMENT ON TABLE companies IS 'Компании, где работают слушатели курсов';
+    `);
+};
+
+exports.down = pgm => {
+    pgm.sql(`
+        DROP TABLE companies;
+    `);
+};
