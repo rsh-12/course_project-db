8f375f3dc0a77a7038bf4a5d538cdc4ad4684e00
rsh-12 <grinvichforum12@mail.ru>
2021-10-24 10:48:06 +0500

    Add ddl script

---
+++ sql/ddl/script.sql
@@ -0,0 +1,13 @@
+-- Таблица компании
+CREATE TABLE companies
+(
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR(30) NOT NULL,
+    description VARCHAR(120),
+    created_at  TIMESTAMP DEFAULT current_timestamp,
+    upated_at   TIMESTAMP DEFAULT current_timestamp
+);
+
+COMMENT ON TABLE companies IS 'Компании, где работают слушатели курсов';
+
+-- Таблица слушатели
