2f1dcb79c0176f09caf0b8884cc0c887546a2400
rsh-12 <grinvichforum12@mail.ru>
2021-10-24 16:55:52 +0500

    Update schema.sql

--- sql/ddl/schema.sql
+++ sql/ddl/schema.sql
@@ -103,4 +103,13 @@ CREATE TABLE certificates
 COMMENT ON TABLE certificates IS 'Сертификаты, выданные студентам';
 
 
-/* ... */
+/* КУРСЫ И ПРЕПОДАВАТЕЛИ */
+CREATE TABLE courses_instructors
+(
+    id            SERIAL PRIMARY KEY,
+    instructor_id INT NOT NULL REFERENCES instructors (id) ON DELETE CASCADE,
+    course_id     INT NOT NULL REFERENCES courses (id) ON DELETE CASCADE,
+    created_at    TIMESTAMP DEFAULT current_timestamp
+);
+
+COMMENT ON TABLE courses_instructors IS 'Курсы и преподаватели';
