03f3b6a12c8bd2c7d386911563ad2889edf06523
rsh-12 <grinvichforum12@mail.ru>
2021-10-24 17:06:38 +0500

    Add contracts table

--- sql/ddl/schema.sql
+++ sql/ddl/schema.sql
@@ -125,3 +125,21 @@ CREATE TABLE courses_students
 );
 
 COMMENT ON TABLE courses_students IS 'Курсы и студенты';
+
+
+/* ДОГОВОРЫ */
+CREATE TABLE contracts
+(
+    id              SERIAL PRIMARY KEY,
+    conclusion_date DATE      DEFAULT current_date,
+    completion_date DATE      DEFAULT current_date + INTERVAL '1 year',
+    company_id      INT NOT NULL REFERENCES companies (id) ON DELETE CASCADE,
+    student_id      INT NOT NULL REFERENCES students (id) ON DELETE CASCADE,
+    instructor_id   INT NOT NULL REFERENCES instructors (id) ON DELETE CASCADE,
+    course_id       INT NOT NULL REFERENCES courses (id) ON DELETE CASCADE,
+    created_at      TIMESTAMP DEFAULT current_timestamp,
+
+    CHECK ( completion_date > conclusion_date )
+);
+
+COMMENT ON TABLE contracts IS 'Договоры, заключенные между сторонами';
