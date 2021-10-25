35a1636e3a73512b94b5ee3134793a5f61eff194
rsh-12 <grinvichforum12@mail.ru>
2021-10-25 21:56:18 +0500

    Add some triggers

---
+++ sql/other/updated_at_triggres.sql
@@ -0,0 +1,32 @@
+CREATE OR REPLACE FUNCTION update_timestamp() RETURNS TRIGGER AS
+$update_timestamp$
+BEGIN
+    new.updated_at := current_timestamp;
+    RETURN new;
+END;
+$update_timestamp$ LANGUAGE plpgsql;
+
+CREATE TRIGGER updated_at_companies
+    BEFORE INSERT OR UPDATE
+    ON companies
+    FOR EACH ROW
+EXECUTE PROCEDURE update_timestamp();
+
+CREATE TRIGGER updated_at_courses
+    BEFORE INSERT OR UPDATE
+    ON courses
+    FOR EACH ROW
+EXECUTE PROCEDURE update_timestamp();
+
+CREATE TRIGGER updated_at_instructors
+    BEFORE INSERT OR UPDATE
+    ON instructors
+    FOR EACH ROW
+EXECUTE PROCEDURE update_timestamp();
+
+CREATE TRIGGER updated_at_students
+    BEFORE INSERT OR UPDATE
+    ON students
+    FOR EACH ROW
+EXECUTE PROCEDURE update_timestamp();
+
