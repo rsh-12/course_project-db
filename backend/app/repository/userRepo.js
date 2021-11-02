b02577a821c3d16cf2c74800b7eb1b141a9cec91
rsh-12 <grinvichforum12@mail.ru>
2021-11-02 16:56:48 +0500

    Update userRepo.js

--- backend/app/repository/userRepo.js
+++ backend/app/repository/userRepo.js
@@ -1,21 +1,24 @@
 const pool = require('../config/pool.confg');
+const toCamelCase = require('./utils/toCamelCase');
 
 class UserRepo {
     static async existsByUsername(username) {
         const {rows} = await pool.query('select exists(select username from users where username = $1);', [username]);
+
         return rows[0].exists;
     }
 
     static async findByUsername(username) {
         const {rows} = await pool.query('select * from users where username = $1', [username]);
-        return rows[0];
+
+        return toCamelCase(rows)[0];
     }
 
     static async insert(username, password) {
         const {rows} = await pool.query('insert into users(username, password) values ($1, $2) returning *;',
             [username, password]);
 
-        return rows[0];
+        return toCamelCase(rows)[0];
     }
 }
 
