09c0a23a87c9a7a16cbdcff9d2539587c2c55788
rsh-12 <grinvichforum12@mail.ru>
2021-11-06 13:43:56 +0500

    Add findByCourseId method

---
+++ backend/app/repository/instructor.repo.js
@@ -0,0 +1,15 @@
+const pool = require('../config/pool.confg');
+const toCamelCase = require('./utils/toCamelCase');
+
+class InstructorRepo {
+    static async findByCourseId(id) {
+        const {rows} = await pool.query(`select *
+                                         from instructors i
+                                                  join courses_instructors ci on i.id = ci.instructor_id
+                                         where course_id = $1;`, [id]);
+
+        return toCamelCase(rows);
+    }
+}
+
+module.exports = InstructorRepo
