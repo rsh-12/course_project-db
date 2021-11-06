b793b69581e56f3e003115e0fca3326c8f02a3a6
rsh-12 <grinvichforum12@mail.ru>
2021-11-06 14:34:28 +0500

    Update getOne method

--- backend/app/contollers/course.controller.js
+++ backend/app/contollers/course.controller.js
@@ -1,15 +1,16 @@
 const CourseRepo = require('../repository/course.repo');
+const InstructorRepo = require('../repository/instructor.repo');
 const cache = require('../config/cache.config');
 
 exports.getAll = async (req, res) => {
     let courses = cache.get('courses');
 
     if (!!courses) {
-        console.debug('from cache')
+        console.debug('courses from cache')
 
         res.send(courses);
     } else {
-        console.debug('from DB')
+        console.debug('courses from DB')
         courses = await CourseRepo.find();
         cache.set('courses', courses, 12 * 60 * 60);
 
@@ -23,8 +24,18 @@ exports.getOne = async (req, res) => {
     const {id} = req.params;
     const course = await CourseRepo.findById(id);
 
+    let instructors = cache.get(`instructors_by_course_${id}`);
+
+    if (!!instructors) {
+        console.debug("instructors from cache");
+    } else {
+        console.debug("instructors from DB");
+        instructors = await InstructorRepo.findByCourseId(id);
+        cache.set(`instructors_by_course_${id}`, instructors, 12 * 60 * 60);
+    }
+
     course
-        ? res.send(course)
+        ? res.send({course, instructors})
         : res.sendStatus(404);
 }
 
@@ -32,8 +43,9 @@ exports.delete = async (req, res) => {
     const {id} = req.params;
     const course = await CourseRepo.delete(id);
     if (course) {
+        cache.del(`instructors_by_course_${id}`);
         cache.del('courses');
-        console.debug('the key "courses" has been deleted')
+        console.debug('the key "courses" has been deleted');
 
         res.send(course);
     } else {
