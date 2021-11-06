9fc36bfcb60804efcd616b42f36d93d70855efbb
rsh-12 <grinvichforum12@mail.ru>
2021-11-06 13:00:32 +0500

    Add findById method

--- backend/app/repository/course.repo.js
+++ backend/app/repository/course.repo.js
@@ -7,6 +7,13 @@ class CourseRepo {
 
         return toCamelCase(rows);
     }
+
+    static async findById(id) {
+        const {rows} = await pool.query('select * from courses where id = $1', [id]);
+
+        return toCamelCase(rows)[0];
+    }
+
 }
 
 module.exports = CourseRepo
