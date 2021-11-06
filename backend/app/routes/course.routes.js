671675a7485cc45047ca0e95f9c8f30164414d15
rsh-12 <grinvichforum12@mail.ru>
2021-11-06 11:57:43 +0500

    Add course.routes.js

---
+++ backend/app/routes/course.routes.js
@@ -0,0 +1,15 @@
+const {authJwt} = require('../middleware');
+const controller = require('../contollers/course.controller');
+
+module.exports = function (app) {
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Headers", "x-access-token, Origin, Content-Type, Accept");
+        next();
+    });
+
+    app.get("/api/courses",
+        // [authJwt.verifyToken], todo: uncomment later
+        controller.getAll
+    );
+
+}
