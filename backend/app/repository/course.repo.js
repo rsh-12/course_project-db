2f0d57bc3a0e9b43ef3387bfc7d9ee6ccfbc9c78
rsh-12 <grinvichforum12@mail.ru>
2021-11-06 21:36:02 +0500

    Add update method

--- backend/app/repository/course.repo.js
+++ backend/app/repository/course.repo.js
@@ -23,14 +23,35 @@ class CourseRepo {
     }
 
     static async insert(name, category, description, hours, startDate, endDate) {
+        const values = [name, category, description, hours, startDate, endDate];
+
         const {rows} = await pool.query(
             `insert into courses(name, category, description, hours, start_date, end_date)
-             values ($1, $2, $3, $4, $5, $6) returning *;`,
-            [name, category, description, hours, startDate, endDate]);
+             values ($1, $2, $3, $4, $5, $6) returning *;`, values);
 
         return toCamelCase(rows)[0];
     }
 
+    static async update(id, name, category, description, hours, startDate, endDate) {
+        const values = [id, name, category, description, hours, startDate, endDate];
+
+        const {rows} = await pool.query(`update courses
+                                         set name        = $2,
+                                             category    = $3,
+                                             description = $4,
+                                             hours       = $5,
+                                             start_date  = $6,
+                                             end_date    = $7
+                                         where id = $1 returning *;`, values);
+
+        return toCamelCase(rows)[0];
+    }
+
+    static async existsByName(name) {
+        const {rows} = await pool.query('select exists(select * from courses where name = $1);', [name]);
+
+        return rows[0].exists;
+    }
 }
 
 module.exports = CourseRepo
