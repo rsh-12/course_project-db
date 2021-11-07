7ffb6d897ca31666be7c9c154b25ca6be1de5e78
rsh-12 <grinvichforum12@mail.ru>
2021-11-07 23:02:40 +0500

    Add AuthGuardService

---
+++ frontend/src/app/services/auth-guard.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core';
+import {AuthService} from "./auth.service";
+import {CanActivate, Router} from "@angular/router";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate {
+
+    constructor(private authService: AuthService, public router: Router) {
+    }
+
+    canActivate(): boolean {
+        console.log('in guard')
+        if (!this.authService.isAuthenticated()) {
+            this.router.navigate(['login']);
+            return false;
+        }
+        return true;
+    }
+
+}
