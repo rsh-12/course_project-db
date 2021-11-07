7d1761f8c12a2871ee13e2f6588f875eb4902fee
rsh-12 <grinvichforum12@mail.ru>
2021-11-07 11:31:19 +0500

    Update statistics

--- backend/app/contollers/common.controller.js
+++ backend/app/contollers/common.controller.js
@@ -1,6 +1,20 @@
+const pool = require('../config/pool.confg');
+const CourseRepo = require('../repository/course.repo');
 
+exports.statistics = async (req, res) => {
+    const {rows} = await pool.query('SELECT * FROM total_records');
+    const totalRecords = rows[0];
 
-exports.statistics = (req, res) => {
-    return res.status(200).send("Secured Content.");
+    const priceInfo = await CourseRepo.priceInfo();
+
+    if (!totalRecords) {
+        return res.status(500).send({message: 'Something went wrong'});
+    }
+
+    return res.send({
+        totalRecords,
+        priceInfo
+    });
+
 };
 
