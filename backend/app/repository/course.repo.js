5723c1de37c5be53caa19e67cc6d00f735379796
rsh-12 <grinvichforum12@mail.ru>
2021-11-07 11:01:31 +0500

    Update repo

--- backend/app/repository/course.repo.js
+++ backend/app/repository/course.repo.js
@@ -3,52 +3,53 @@ const toCamelCase = require('./utils/toCamelCase');
 
 class CourseRepo {
     static async find() {
-        const {rows} = await pool.query('select * from courses_info;');
+        const {rows} = await pool.query('SELECT * FROM courses_info;');
 
         return toCamelCase(rows);
     }
 
     static async findById(id) {
-        const {rows} = await pool.query('select * from courses where id = $1;', [id]);
+        const {rows} = await pool.query('SELECT * FROM courses WHERE id = $1;', [id]);
 
         return toCamelCase(rows)[0];
     }
 
     static async delete(id) {
-        const {rows} = await pool.query(`delete
-                                         from courses
-                                         where id = $1 returning *;`, [id]);
+        const {rows} = await pool.query(`DELETE
+                                         FROM courses
+                                         WHERE id = $1 returning *;`, [id]);
 
         return toCamelCase(rows)[0];
     }
 
-    static async insert(name, category, description, hours, startDate, endDate) {
-        const values = [name, category, description, hours, startDate, endDate];
+    static async insert(name, category, description, hours, startDate, endDate, price) {
 
         const {rows} = await pool.query(
-            `insert into courses(name, category, description, hours, start_date, end_date)
-             values ($1, $2, $3, $4, $5, $6) returning *;`, values);
+            `INSERT INTO courses(name, category, description, hours, start_date, end_date, price)
+             VALUES ($1, $2, $3, $4, $5, $6, $7) returning *;`,
+            [name, category, description, hours, startDate, endDate, price]);
 
         return toCamelCase(rows)[0];
     }
 
-    static async update(id, name, category, description, hours, startDate, endDate) {
-        const values = [id, name, category, description, hours, startDate, endDate];
+    static async update(id, name, category, description, hours, startDate, endDate, price) {
 
-        const {rows} = await pool.query(`update courses
-                                         set name        = $2,
+        const {rows} = await pool.query(`UPDATE courses
+                                         SET name        = $2,
                                              category    = $3,
                                              description = $4,
                                              hours       = $5,
                                              start_date  = $6,
-                                             end_date    = $7
-                                         where id = $1 returning *;`, values);
+                                             end_date    = $7,
+                                             price       = $8
+                                         WHERE id = $1 returning *;`,
+            [id, name, category, description, hours, startDate, endDate, price]);
 
         return toCamelCase(rows)[0];
     }
 
     static async existsByName(name) {
-        const {rows} = await pool.query('select exists(select * from courses where name = $1);', [name]);
+        const {rows} = await pool.query('SELECT EXISTS(SELECT * FROM courses WHERE name = $1);', [name]);
 
         return rows[0].exists;
     }
