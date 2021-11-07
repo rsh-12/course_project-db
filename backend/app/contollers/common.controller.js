3bd3987c4a93ca3886c949e01b7cf77d0e35a35b
rsh-12 <grinvichforum12@mail.ru>
2021-11-07 10:57:40 +0500

    Rename user to common

---
+++ backend/app/contollers/common.controller.js
@@ -0,0 +1,6 @@
+
+
+exports.statistics = (req, res) => {
+    return res.status(200).send("Secured Content.");
+};
+
--- backend/app/contollers/user.controller.js
+++
@@ -1,8 +0,0 @@
-exports.allAccess = (req, res) => {
-    return res.status(200).send("Public Content.");
-};
-
-exports.securedContent = (req, res) => {
-    return res.status(200).send("Secured Content.");
-};
-
