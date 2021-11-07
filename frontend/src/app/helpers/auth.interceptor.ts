3f22096c094b04ec2fd692a7bf7ca002e12ac4f4
rsh-12 <grinvichforum12@mail.ru>
2021-11-08 00:04:01 +0500

    Fix typo, rename token

--- frontend/src/app/helpers/auth.interceptor.ts
+++ frontend/src/app/helpers/auth.interceptor.ts
@@ -4,24 +4,24 @@ import {HttpInterceptor, HttpHandler, HttpRequest} from "@angular/common/http";
 import {TokenStorageService} from "../services/token-storage.service";
 import {Observable} from "rxjs";
 
-const TOKEN_HEADER_KEY = "X-access-token"
+const TOKEN_HEADER_KEY = "x-access-token"
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-    constructor(private token: TokenStorageService) {
+    constructor(private tokenStorage: TokenStorageService) {
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         let authReq = req;
-        const token = this.token.getToken();
+        const token = this.tokenStorage.getToken();
         if (token != null) {
-            authReq = req.clone({headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token)})
+            authReq = req.clone({headers: req.headers.set(TOKEN_HEADER_KEY, token)});
         }
         return next.handle(authReq);
     }
 }
 
 export const authInterceptorProviders = [
-    {provider: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
 ];
