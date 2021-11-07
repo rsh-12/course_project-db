8d540fe14d2369fe6f2638f056aa73c96f1092b6
rsh-12 <grinvichforum12@mail.ru>
2021-11-08 01:32:43 +0500

    Update styleUrls

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -5,7 +5,7 @@ import {TokenStorageService} from "../../services/token-storage.service";
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss']
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
