c601a328bef534bae3dc1387c1233093f13e203b
rsh-12 <grinvichforum12@mail.ru>
2021-11-08 01:33:19 +0500

    Update styleUrls

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -4,7 +4,7 @@ import {TokenStorageService} from "../../services/token-storage.service";
 @Component({
     selector: 'app-profile',
     templateUrl: './profile.component.html',
-    styleUrls: ['./profile.component.scss']
+    styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
     currentUser: any;
--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -4,7 +4,7 @@ import {AuthService} from "../../services/auth-service";
 @Component({
     selector: 'app-register',
     templateUrl: './register.component.html',
-    styleUrls: ['./register.component.scss']
+    styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
 
