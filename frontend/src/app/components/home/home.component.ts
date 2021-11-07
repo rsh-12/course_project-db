2ecef9847daf2b33e8c33e5f28d8a0104d6edd5c
rsh-12 <grinvichforum12@mail.ru>
2021-11-08 01:32:30 +0500

    Update home.component.ts

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,23 +1,22 @@
 import {Component, OnInit} from '@angular/core';
 import {CommonService} from "../../services/common-service";
+import {Statistics} from "../../common/statistics";
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss']
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-    content?: string;
     errorMessage = '';
+    stats!: Statistics;
 
     constructor(private commonService: CommonService) {
     }
 
     ngOnInit(): void {
         this.commonService.getStatistics().subscribe(
-            data => {
-                console.log(data)
-            },
+            data => this.stats = data,
             err => {
                 console.log(err.error.message);
                 this.errorMessage = err.error.message;
