b44f4e107f70abdde10eccc55134d0e8d31a328e
rsh-12 <grinvichforum12@mail.ru>
2021-11-08 21:40:12 +0500

    Add findById method

--- backend/app/repository/user.repo.js
+++ backend/app/repository/user.repo.js
@@ -20,6 +20,12 @@ class UserRepo {
 
         return toCamelCase(rows)[0];
     }
+
+    static async findById(id) {
+        const {rows} = await pool.query('SELECT * FROM users WHERE id = $1;', [id]);
+
+        return toCamelCase(rows)[0];
+    }
 }
 
 module.exports = UserRepo;
