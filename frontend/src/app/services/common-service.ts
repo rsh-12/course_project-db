596f9142333831d984655b40d59d14d3de1759b3
rsh-12 <grinvichforum12@mail.ru>
2021-11-08 21:40:53 +0500

    Add whoAmI method

--- frontend/src/app/services/common-service.ts
+++ frontend/src/app/services/common-service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Statistics} from "../common/statistics";
+import {CurrentUser} from "../common/currentUser";
 
 const API_URL = 'http://localhost:8080/api/';
 
@@ -15,6 +16,10 @@ export class CommonService {
 
     getStatistics(): Observable<Statistics> {
         return this.http.get<Statistics>(API_URL + 'statistics');
+    }
+
+    whoAmI(): Observable<CurrentUser> {
+        return this.http.get<CurrentUser>(API_URL + 'whoami');
     }
 
 }
