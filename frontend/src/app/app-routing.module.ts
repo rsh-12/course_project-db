45035b082ffc13e30b860c28a0afa7fb3003379d
rsh-12 <grinvichforum12@mail.ru>
2021-11-09 15:55:31 +0500

    Add courses routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,13 +6,19 @@ import {LoginComponent} from "./components/login/login.component";
 import {HomeComponent} from "./components/home/home.component";
 import {ProfileComponent} from "./components/profile/profile.component";
 import {AuthGuardService} from "./services/auth-guard.service";
+import {CoursesListComponent} from "./components/courses-list/courses-list.component";
+import {CourseDetailsComponent} from "./components/course-details/course-details.component";
+import {AddCourseComponent} from "./components/add-course/add-course.component";
 
 const routes: Routes = [
     {path: 'home', component: HomeComponent, canActivate: [AuthGuardService]},
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
     {path: 'profile', component: ProfileComponent, canActivate: [AuthGuardService]},
-    {path: '', redirectTo: 'home', pathMatch: 'full'}
+    {path: 'courses', component: CoursesListComponent, canActivate: [AuthGuardService]},
+    {path: 'courses/:id', component: CourseDetailsComponent, canActivate: [AuthGuardService]},
+    {path: 'courses/add', component: AddCourseComponent, canActivate: [AuthGuardService]},
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
 ];
 
 @NgModule({
