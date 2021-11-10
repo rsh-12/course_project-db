c7e89ac63d08870d20b35581e34d10149e8189bb
rsh-12 <grinvichforum12@mail.ru>
2021-11-10 08:03:42 +0500

    Add courses-list component and update it

---
+++ frontend/src/app/components/courses-list/courses-list.component.ts
@@ -0,0 +1,65 @@
+import {Component, OnInit} from '@angular/core';
+import {CourseInfo} from "../../common/courseInfo";
+import {CourseService} from "../../services/course-service";
+
+@Component({
+    selector: 'app-courses-list',
+    templateUrl: './courses-list.component.html',
+    styleUrls: ['./courses-list.component.css']
+})
+export class CoursesListComponent implements OnInit {
+
+    courses?: CourseInfo[];
+    totalCourses = 0;
+    currentCourse: CourseInfo = {};
+    currentIndex = -1;
+    public name = '';
+
+    constructor(private courseService: CourseService) {
+    }
+
+    ngOnInit(): void {
+        this.retrieveCourses();
+    }
+
+    private retrieveCourses() {
+        this.courseService.getAll().subscribe(
+            data => {
+                this.totalCourses = data.length
+                this.courses = data;
+            }, error => {
+                this.totalCourses = 0;
+                console.log(error);
+            }
+        )
+    }
+
+    private refreshList() {
+        this.retrieveCourses();
+        this.currentCourse = {};
+        this.currentIndex = -1;
+    }
+
+    searchByName() {
+        this.currentCourse = {};
+        this.currentIndex = -1;
+
+        console.log('this.name: ' + this.name);
+
+        this.courseService.findByName(this.name).subscribe(
+            data => {
+                this.totalCourses = data.length
+                this.courses = data;
+                this.name = ''
+            }, error => {
+                console.log(error);
+            }
+        )
+    }
+
+    setActiveCourse(course: CourseInfo, i: number) {
+        this.currentCourse = course;
+        this.currentIndex = i;
+    }
+
+}
