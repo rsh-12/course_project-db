2b74f3f3343db064454153363959315a2ec7d7ef
rsh-12 <grinvichforum12@mail.ru>
2021-11-10 12:00:10 +0500

    Add getCourseById method

--- frontend/src/app/components/course-details/course-details.component.ts
+++ frontend/src/app/components/course-details/course-details.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Course} from "../../common/course";
+import {CourseService} from "../../services/course-service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Instructor} from "../../common/instructor";
+import {CourseById} from "../../common/courseById";
 
 @Component({
-  selector: 'app-course-details',
-  templateUrl: './course-details.component.html',
-  styleUrls: ['./course-details.component.css']
+    selector: 'app-course-details',
+    templateUrl: './course-details.component.html',
+    styleUrls: ['./course-details.component.css']
 })
 export class CourseDetailsComponent implements OnInit {
 
-  constructor() { }
+    course: Course = {
+        name: '',
+        category: '',
+        description: '',
+        hours: 0,
+        startDate: new Date(),
+        endDate: new Date(),
+        price: 0.00
+    }
 
-  ngOnInit(): void {
-  }
+    instructors: Instructor[] = [];
+    totalStudents = 0;
 
+    response: CourseById = {
+        course: this.course,
+        instructors: this.instructors,
+        totalStudents: this.totalStudents
+    }
+
+    message = '';
+
+    constructor(private courseService: CourseService,
+                private route: ActivatedRoute,
+                private router: Router) {
+    }
+
+    ngOnInit(): void {
+        this.message = '';
+        this.getCourseById(this.route.snapshot.params.id);
+    }
+
+    private getCourseById(id: string) {
+        this.courseService.findById(id).subscribe(
+            data => {
+                this.response = data;
+                console.log(data);
+            }, error => {
+                console.log(error);
+            }
+        )
+    }
 }
