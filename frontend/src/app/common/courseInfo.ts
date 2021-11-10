134e11f67c8aa37f163a47c9bc7739db3cb98e0b
rsh-12 <grinvichforum12@mail.ru>
2021-11-10 08:05:27 +0500

    Add courseInfo.ts

---
+++ frontend/src/app/common/courseInfo.ts
@@ -0,0 +1,12 @@
+export class CourseInfo {
+    id?: number;
+    name?: string;
+    category?: string;
+    startDate?: Date;
+    endDate?: Date;
+    updatedAt?: Date;
+    firstName?: string;
+    lastName?: string
+    studentsCount?: string;
+}
+
