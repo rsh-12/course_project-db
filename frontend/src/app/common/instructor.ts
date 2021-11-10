5f6f38862fdc0ab3f5fbc4a78e5f7522d96d2eae
rsh-12 <grinvichforum12@mail.ru>
2021-11-10 11:52:10 +0500

    Add instructor.ts

---
+++ frontend/src/app/common/instructor.ts
@@ -0,0 +1,9 @@
+export class Instructor {
+    id?: number;
+    firstName?: string;
+    lastName?: string;
+    education?: string;
+    degree?: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
