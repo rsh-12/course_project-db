fa3bd4afbd203c02821a397d576dd2a0678326f4
rsh-12 <grinvichforum12@mail.ru>
2021-11-10 11:59:46 +0500

    Add findById method

--- frontend/src/app/services/course-service.ts
+++ frontend/src/app/services/course-service.ts
@@ -2,6 +2,7 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {CourseInfo} from "../common/courseInfo";
+import {CourseById} from "../common/courseById";
 
 const API_URL = 'http://localhost:8080/api/courses/';
 
@@ -17,7 +18,11 @@ export class CourseService {
         return this.http.get<CourseInfo[]>(API_URL);
     }
 
-    findByName(name: string) {
+    findByName(name: string): Observable<CourseInfo[]> {
         return this.http.get<CourseInfo[]>(API_URL + '?name=' + name);
+    }
+
+    findById(id: string): Observable<CourseById> {
+        return this.http.get<CourseById>(API_URL + id);
     }
 }
