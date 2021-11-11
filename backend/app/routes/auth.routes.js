b82c77fa899e29c2f7680b769bca67461a7ad770
rsh-12 <grinvichforum12@mail.ru>
2021-11-11 18:03:05 +0500

    Replace named func with arrow func

--- backend/app/routes/auth.routes.js
+++ backend/app/routes/auth.routes.js
@@ -1,8 +1,8 @@
 const {verifySignUp} = require('../middleware');
 const controller = require('../contollers/auth.controller');
 
-module.exports = function (app) {
-    app.use(function (req, res, next) {
+module.exports = app => {
+    app.use((req, res, next) => {
         res.header(
             "Access-Control-Allow-Headers",
             "x-access-token, Origin, Content-Type, Accept"
--- backend/app/routes/course.routes.js
+++ backend/app/routes/course.routes.js
@@ -2,8 +2,8 @@ const {authJwt} = require('../middleware');
 const {validateRequest} = require('../middleware');
 const controller = require('../contollers/course.controller');
 
-module.exports = function (app) {
-    app.use(function (req, res, next) {
+module.exports = app => {
+    app.use((req, res, next) => {
         res.header("Access-Control-Allow-Headers", "x-access-token, Origin, Content-Type, Accept");
         next();
     });
