c28eb0f3792efbb89a2ab5c037b59c47783e19c0
rsh-12 <grinvichforum12@mail.ru>
2021-11-11 17:29:34 +0500

    Add certificates-list component

---
+++ frontend/src/app/components/certificates/certificates-list/certificates-list.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CertificatesListComponent } from './certificates-list.component';
+
+describe('CertificatesListComponent', () => {
+  let component: CertificatesListComponent;
+  let fixture: ComponentFixture<CertificatesListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CertificatesListComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CertificatesListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
