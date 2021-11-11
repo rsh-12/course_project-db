d594b9621a58d5cd4d3d38ba0e793c68853bc28b
rsh-12 <grinvichforum12@mail.ru>
2021-11-11 18:19:30 +0500

    Add get companies route

--- backend/app/routes/company.routes.js
+++ backend/app/routes/company.routes.js
@@ -1,6 +1,15 @@
+const {authJwt} = require("../middleware");
+const controller = require('../contollers/company.controller');
+
 module.exports = app => {
     app.use((req, res, next) => {
         res.header("Access-Control-Allow-Headers", "x-access-token, Origin, Content-Type, Accept");
         next();
     });
+
+    app.get("/api/companies",
+        [authJwt.verifyToken],
+        controller.getAll
+    );
+
 }
