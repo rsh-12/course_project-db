fd91cdd27f955029d33305cab906fa6fea6f8235
rsh-12 <grinvichforum12@mail.ru>
2021-11-11 17:53:34 +0500

    Rename and update totalRecords.ts

---
+++ frontend/src/app/common/totalRecords.ts
@@ -0,0 +1,9 @@
+export class TotalRecords {
+    companies: number = 0;
+    students: number = 0;
+    contracts: number = 0;
+    courses: number = 0;
+    instructors: number = 0;
+    certificates: number = 0;
+}
+
