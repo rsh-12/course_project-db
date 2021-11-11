8507d19e4f8202cc15df82ae583109ba193e7cab
rsh-12 <grinvichforum12@mail.ru>
2021-11-11 17:36:09 +0500

    Delete priceInfo query

--- backend/app/contollers/common.controller.js
+++ backend/app/contollers/common.controller.js
@@ -1,21 +1,15 @@
 const pool = require('../config/pool.confg');
-const CourseRepo = require('../repository/course.repo');
 const UserRepo = require('../repository/user.repo');
 
 exports.statistics = async (req, res) => {
     const {rows} = await pool.query('SELECT * FROM total_records');
     const totalRecords = rows[0];
 
-    const priceInfo = await CourseRepo.priceInfo();
-
     if (!totalRecords) {
         return res.status(500).send({message: 'Something went wrong'});
     }
 
-    return res.send({
-        totalRecords,
-        priceInfo
-    });
+    return res.send(totalRecords);
 };
 
 exports.whoami = async (req, res) => {
