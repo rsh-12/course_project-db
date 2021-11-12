94270636642610a1669ef7da6398cddbe9d3fb5f
rsh-12 <grinvichforum12@mail.ru>
2021-11-12 11:40:56 +0500

    Add findAll method

--- frontend/src/app/services/company-service.ts
+++ frontend/src/app/services/company-service.ts
@@ -1,5 +1,7 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Company} from "../common/company";
 
 const API_URL = 'http://localhost:8080/api/companies/';
 
@@ -11,7 +13,9 @@ export class CompanyService {
     constructor(private http: HttpClient) {
     }
 
-
+    findAll(): Observable<Company[]> {
+        return this.http.get<Company[]>(API_URL);
+    }
 
 
 }
