8f21b4a747d03039629c0c9f6331ac96a8154ca5
rsh-12 <grinvichforum12@mail.ru>
2021-11-12 12:18:55 +0500

    Add student.ts

---
+++ frontend/src/app/common/student.ts
@@ -0,0 +1,11 @@
+export class Student {
+    id?: number;
+    firstName?: string;
+    lastName?: string;
+    dateOfBirth?: Date;
+    phone?: string;
+    email?: string;
+    companyId?: number;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
