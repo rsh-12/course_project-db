728cf35a6eb0c503daefe3584617ab58bc7c10e3
rsh-12 <grinvichforum12@mail.ru>
2021-11-13 06:36:06 +0500

    Add retrieveStudents method

--- frontend/src/app/components/students/students-list/students-list.component.ts
+++ frontend/src/app/components/students/students-list/students-list.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {StudentService} from "../../../services/student-service";
+import {Student} from "../../../common/student";
 
 @Component({
-  selector: 'app-students-list',
-  templateUrl: './students-list.component.html',
-  styleUrls: ['./students-list.component.css']
+    selector: 'app-students-list',
+    templateUrl: './students-list.component.html',
+    styleUrls: ['./students-list.component.css']
 })
 export class StudentsListComponent implements OnInit {
 
-  constructor() { }
+    students: Student[] = [];
+    loading = false;
 
-  ngOnInit(): void {
-  }
+    constructor(private studentService: StudentService) {
+    }
+
+    ngOnInit(): void {
+        this.loading = true;
+        this.retrieveStudents();
+    }
+
+    private retrieveStudents() {
+        this.studentService.findAll().subscribe(
+            data => {
+                this.students = data;
+                this.loading = false;
+            }, error => {
+                console.log(error);
+                this.loading = false;
+            }
+        );
+    }
 
 }
