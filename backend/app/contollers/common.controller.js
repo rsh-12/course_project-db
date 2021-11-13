21fcee7e36ad64481e3e97fac47ee00ada8de09f
rsh-12 <grinvichforum12@mail.ru>
2021-11-13 11:12:09 +0500

    Add contracts function

--- backend/app/contollers/common.controller.js
+++ backend/app/contollers/common.controller.js
@@ -1,5 +1,7 @@
 const pool = require('../config/pool.confg');
 const UserRepo = require('../repository/user.repo');
+const toCamelCase = require('../repository/utils/toCamelCase');
+const cache = require('../config/cache.config');
 
 exports.statistics = async (req, res) => {
     const {rows} = await pool.query('SELECT * FROM total_records');
@@ -20,3 +22,24 @@ exports.whoAmI = async (req, res) => {
         : res.sendStatus(404);
 }
 
+exports.contracts = async (req, res) => {
+    let contracts = cache.get('contracts');
+
+    if (!!contracts) {
+        console.log('contracts from cache');
+        return res.send(contracts);
+    }
+
+    const {rows} = await pool.query('SELECT * FROM show_contracts();');
+    console.log(`> show_contracts(): ${rows.length}`);
+
+    contracts = toCamelCase(rows);
+
+    if (contracts) {
+        cache.set('contracts', contracts, 12 * 60 * 60);
+        console.log('contracts from DB');
+        return res.send(contracts);
+    }
+
+    return res.status(404).send({message: 'Contracts not found or something went wrong'});
+}
