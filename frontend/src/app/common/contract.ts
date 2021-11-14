6de15d0d8ab2f971eb18f62297e229ad9e12c838
rsh-12 <grinvichforum12@mail.ru>
2021-11-14 21:54:16 +0500

    Add contract.ts

---
+++ frontend/src/app/common/contract.ts
@@ -0,0 +1,12 @@
+export class Contract {
+    id!: number;
+    company!: string;
+    studentFirstName!: string;
+    studentLastName!: string;
+    course!: string;
+    category!: string;
+    instructorFirstName!: string;
+    instructorLastName!: string;
+    conclusionDate?: Date;
+    completionDate?: Date;
+}
