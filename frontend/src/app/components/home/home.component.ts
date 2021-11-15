d4c8a85284facbcddbdf06411286a320f0a5dd9c
rsh-12 <grinvichforum12@mail.ru>
2021-11-15 23:54:31 +0500

    Remove totalRecords initialization

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,6 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {CommonService} from "../../services/common-service";
-import {TotalRecords} from "../../common/totalRecords";
 
 @Component({
     selector: 'app-home',
@@ -8,20 +6,10 @@ import {TotalRecords} from "../../common/totalRecords";
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-    errorMessage = '';
-    totalRecords!: TotalRecords;
-
-    constructor(private commonService: CommonService) {
+    constructor() {
     }
 
     ngOnInit(): void {
-        this.commonService.getStatistics().subscribe(
-            data => this.totalRecords = data,
-            err => {
-                console.log(err.error.message);
-                this.errorMessage = err.error.message;
-            }
-        )
     }
 
 }
