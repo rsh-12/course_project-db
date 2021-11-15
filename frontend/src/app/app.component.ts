4277e6309dfd2bb5497a65adf6cfbb7d1869d15c
rsh-12 <grinvichforum12@mail.ru>
2021-11-16 01:05:55 +0500

    Remove some code

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {TokenStorageService} from "./services/token-storage.service";
-import {MatBottomSheet} from "@angular/material/bottom-sheet";
-import {ButtonSheetComponent} from "./components/button-sheet/button-sheet.component";
 
 @Component({
     selector: 'app-root',
@@ -10,29 +7,11 @@ import {ButtonSheetComponent} from "./components/button-sheet/button-sheet.compo
 })
 export class AppComponent implements OnInit {
     title = 'courses';
-    isLoggedIn = false;
-    username?: string;
 
-    constructor(private tokenStorageService: TokenStorageService,
-                private bottomSheet: MatBottomSheet) {
-    }
-
-    openBottomSheet() {
-        this.bottomSheet.open(ButtonSheetComponent);
+    constructor() {
     }
 
     ngOnInit(): void {
-        this.isLoggedIn = !!this.tokenStorageService.getToken();
-
-        if (this.isLoggedIn) {
-            const user = this.tokenStorageService.getUser();
-            this.username = user.username;
-        }
-    }
-
-    logout() {
-        this.tokenStorageService.signOut();
-        window.location.reload();
     }
 
 }
