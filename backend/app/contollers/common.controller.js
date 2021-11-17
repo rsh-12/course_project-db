e3181b1a0ccb14c8846672df18a71368c4eb9409
rsh-12 <grinvichforum12@mail.ru>
2021-11-17 11:59:21 +0500

    Add income controller

--- backend/app/contollers/common.controller.js
+++ backend/app/contollers/common.controller.js
@@ -53,3 +53,43 @@ exports.contracts = async (req, res) => {
 
     return res.status(404).send({message: 'Contracts not found or something went wrong'});
 }
+
+exports.income = async (req, res) => {
+    let income = cache.get('income');
+
+    if (!!income) {
+        console.log('income from cache');
+        return res.send(income);
+    }
+
+    const {rows} = await pool.query(`
+        SELECT 'month' AS type, SUM(c2.price) AS value
+        FROM contracts c
+                 JOIN courses_instructors ci ON ci.id = c.course_instructor_id
+                 JOIN courses c2 ON c2.id = ci.course_id
+        WHERE c2.start_date BETWEEN CURRENT_DATE - INTERVAL '1 mon' AND CURRENT_DATE
+        UNION ALL
+        SELECT 'year', SUM(c2.price)
+        FROM contracts c
+                 JOIN courses_instructors ci ON ci.id = c.course_instructor_id
+                 JOIN courses c2 ON c2.id = ci.course_id
+        WHERE c2.start_date BETWEEN CURRENT_DATE - INTERVAL '1 year' AND CURRENT_DATE
+        UNION ALL
+        SELECT 'percentage',
+               (SELECT COUNT(*)
+                FROM students
+                WHERE NOT EXISTS(SELECT 1 FROM contracts WHERE student_id = students.id)) * 100
+                   /
+               (SELECT COUNT(*) FROM students) AS percentages
+        UNION ALL
+        SELECT 'withoutContracts', COUNT(*)
+        FROM students
+        WHERE NOT EXISTS(SELECT 1 FROM contracts WHERE student_id = students.id)
+    `);
+
+    income = rows;
+    console.log('income from DB');
+    cache.set('income', income);
+
+    return res.send(income);
+}
