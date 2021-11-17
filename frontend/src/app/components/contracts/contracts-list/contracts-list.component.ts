bc3212f70696265d9baf52d96c4e65852cb89c48
rsh-12 <grinvichforum12@mail.ru>
2021-11-17 18:13:01 +0500

    Add retrieveContracts method

--- frontend/src/app/components/contracts/contracts-list/contracts-list.component.ts
+++ frontend/src/app/components/contracts/contracts-list/contracts-list.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CommonService} from "../../../services/common-service";
+import {Contract} from "../../../common/contract";
 
 @Component({
-  selector: 'app-contracts-list',
-  templateUrl: './contracts-list.component.html',
-  styleUrls: ['./contracts-list.component.css']
+    selector: 'app-contracts-list',
+    templateUrl: './contracts-list.component.html',
+    styleUrls: ['./contracts-list.component.css']
 })
 export class ContractsListComponent implements OnInit {
 
-  constructor() { }
+    contracts: Contract[] = [];
+    loading = false;
 
-  ngOnInit(): void {
-  }
+    constructor(private commonService: CommonService) {
+    }
 
+    ngOnInit(): void {
+        this.loading = true;
+        this.retrieveContracts();
+        this.loading = false;
+
+    }
+
+    private retrieveContracts() {
+        this.commonService.getContracts().subscribe(
+            data => {
+                this.contracts = data;
+            }, error => {
+                console.log(error);
+            }
+        )
+    }
 }
