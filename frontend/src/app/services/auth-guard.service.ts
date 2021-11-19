524608aa53459f929c76bddd7a2800ec850c6ce9
rsh-12 <grinvichforum12@mail.ru>
2021-11-19 19:14:42 +0500

    Delete log message

--- frontend/src/app/services/auth-guard.service.ts
+++ frontend/src/app/services/auth-guard.service.ts
@@ -11,7 +11,6 @@ export class AuthGuardService implements CanActivate {
     }
 
     canActivate(): boolean {
-        console.log('in guard')
         if (!this.authService.isAuthenticated()) {
             this.router.navigate(['login']);
             return false;
