fe03718bd2dd4c9b96324fc31a86c037fe418797
rsh-12 <grinvichforum12@mail.ru>
2021-11-19 20:15:38 +0500

    Update instructors-list component

--- frontend/src/app/components/instructors/instructors-list/instructors-list.component.ts
+++ frontend/src/app/components/instructors/instructors-list/instructors-list.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {InstructorService} from "../../../services/instructor.service";
+import {Instructor} from "../../../common/instructor";
 
 @Component({
-  selector: 'app-instructors-list',
-  templateUrl: './instructors-list.component.html',
-  styleUrls: ['./instructors-list.component.css']
+    selector: 'app-instructors-list',
+    templateUrl: './instructors-list.component.html',
+    styleUrls: ['./instructors-list.component.css']
 })
 export class InstructorsListComponent implements OnInit {
 
-  constructor() { }
+    instructors: Instructor[] = [];
+    loading = false;
 
-  ngOnInit(): void {
-  }
+    constructor(private instructorService: InstructorService) {
+    }
 
+    ngOnInit(): void {
+        this.loading = true;
+        this.retrieveInstructors();
+    }
+
+    private retrieveInstructors() {
+        this.instructorService.findAll().subscribe(
+            data => {
+                this.instructors = data;
+            }, error => {
+                console.log(error);
+            },
+            () => this.loading = false
+        );
+    }
 }
