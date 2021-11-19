ca9c7e3be58b608616276eb73727a232bfc00e76
rsh-12 <grinvichforum12@mail.ru>
2021-11-19 19:34:54 +0500

    Add find method

--- backend/src/repository/instructor.repo.js
+++ backend/src/repository/instructor.repo.js
@@ -2,6 +2,19 @@ const pool = require('../config/pool.confg');
 const toCamelCase = require('./utils/toCamelCase');
 
 class InstructorRepo {
+    static async find() {
+        const {rows} = await pool.query(`
+            SELECT i.*, COUNT(ci.*) courses
+            FROM instructors i
+                     JOIN courses_instructors ci ON i.id = ci.instructor_id
+            GROUP BY i.id, first_name, last_name, education, degree, i.created_at, updated_at;
+        `);
+
+        console.log(`> InstructorRepo.find(): ${rows.length}`);
+
+        return toCamelCase(rows);
+    }
+
     static async findByCourseId(id) {
         const {rows} = await pool.query(`SELECT i.*
                                          FROM instructors i
