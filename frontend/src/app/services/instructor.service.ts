4c279102c1768db875426645182736987e204275
rsh-12 <grinvichforum12@mail.ru>
2021-11-19 20:07:11 +0500

    Add findAll method

--- frontend/src/app/services/instructor.service.ts
+++ frontend/src/app/services/instructor.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Instructor} from "../common/instructor";
+
+const API_URL = 'http://localhost:8080/api/instructors/';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class InstructorService {
 
-  constructor() { }
+    constructor(private http: HttpClient) {
+    }
+
+    findAll(): Observable<Instructor[]> {
+        return this.http.get<Instructor[]>(API_URL);
+    }
+
 }
