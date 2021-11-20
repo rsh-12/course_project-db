0f82219a40cd217b1712101b93588e95fd31254d
rsh-12 <grinvichforum12@mail.ru>
2021-11-20 23:10:28 +0500

    Remove unused import

--- frontend/src/app/helpers/auth.interceptor.ts
+++ frontend/src/app/helpers/auth.interceptor.ts
@@ -1,4 +1,4 @@
-import {HTTP_INTERCEPTORS, HttpEvent, HttpHeaders} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpEvent} from "@angular/common/http";
 import {Injectable} from "@angular/core";
 import {HttpInterceptor, HttpHandler, HttpRequest} from "@angular/common/http";
 import {TokenStorageService} from "../services/token-storage.service";
