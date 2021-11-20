fecb9eda726502d2c1523df99df6f3814c916a75
rsh-12 <grinvichforum12@mail.ru>
2021-11-20 23:05:36 +0500

    Add confirm-dialog component

---
+++ frontend/src/app/components/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-confirm-dialog',
+    template: `
+        <h2 mat-dialog-title>Warning!</h2>
+        <mat-dialog-content class="mat-typography">
+            <p>Are you sure you want to delete this object?</p>
+            <p>After deletion, the object cannot be restored!</p>
+        </mat-dialog-content>
+
+        <mat-dialog-actions align="end">
+            <button mat-button [matDialogClose]="false" mat-dialog-close>Cancel</button>
+            <button mat-button [mat-dialog-close]="true" cdkFocusInitial>Delete</button>
+        </mat-dialog-actions>
+    `,
+    styles: []
+})
+export class ConfirmDialogComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+}
