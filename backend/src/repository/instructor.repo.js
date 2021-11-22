accb87f2585487fb99fa8feb0fc0257f10051ecf
rsh-12 <grinvichforum12@mail.ru>
2021-11-23 00:24:35 +0500

    Replace join with left join, add two methods

--- backend/src/repository/instructor.repo.js
+++ backend/src/repository/instructor.repo.js
@@ -1,12 +1,13 @@
-const pool = require('../config/pool.confg');
+const pool = require('../config/pool.config');
 const toCamelCase = require('./utils/toCamelCase');
+const format = require('pg-format');
 
 class InstructorRepo {
     static async find() {
         const {rows} = await pool.query(`
             SELECT i.*, COUNT(ci.*) courses
             FROM instructors i
-                     JOIN courses_instructors ci ON i.id = ci.instructor_id
+                     LEFT JOIN courses_instructors ci ON i.id = ci.instructor_id
             GROUP BY i.id, first_name, last_name, education, degree, i.created_at, updated_at;
         `);
 
@@ -24,6 +25,61 @@ class InstructorRepo {
 
         return toCamelCase(rows);
     }
+
+    static async findNameAndIdByCourse(id) {
+        const {rows} = await pool.query(
+            `SELECT i.id                                   AS id,
+                    CONCAT(i.last_name, ' ', i.first_name) AS name
+             FROM instructors i
+                      JOIN courses_instructors ci ON i.id = ci.instructor_id
+             WHERE course_id = $1;`, [id]);
+        console.log(`> InstructorRepo.findNameAndIdByCourse(${id}): ${rows.length}`)
+
+        return toCamelCase(rows);
+    }
+
+    static async findNameAndIdExceptCourse(id) {
+        const {rows} = await pool.query(
+            `SELECT i.id                                   AS id,
+                    CONCAT(i.last_name, ' ', i.first_name) AS name
+             FROM instructors i
+             WHERE NOT EXISTS(SELECT instructor_id
+                              FROM courses_instructors
+                              WHERE instructor_id = i.id
+                                AND course_id = $1);`, [id]);
+        console.log(`> InstructorRepo.findNameAndIdExceptCourse(${id}): ${rows.length}`)
+
+        return toCamelCase(rows);
+    }
+
+    static async removeFromCourse(courseId, instructorIds) {
+        const {rows} = await pool.query(`
+                    DELETE
+                    FROM courses_instructors
+                    WHERE course_id = $1
+                      AND instructor_id = ANY ($2::INT[])
+                    RETURNING *;`,
+            [courseId, instructorIds]);
+
+        console.log(`> InstructorRepo.removeFromCourse(${courseId}, ${instructorIds}): ${rows.length}`);
+
+        return toCamelCase(rows);
+    }
+
+    static async addToCourse(courseId, instructorIds) {
+        let values = [];
+        for (let i of instructorIds) values.push([+courseId, +i]);
+
+        console.log(values)
+
+        const {rows} = await pool.query(format(
+            'INSERT INTO courses_instructors (course_id, instructor_id) VALUES %L', values));
+
+        console.log(`> InstructorRepo.addToCourse(${courseId}, ${instructorIds}): ${rows.length}`);
+
+        return toCamelCase(rows);
+    }
+
 }
 
 module.exports = InstructorRepo
