74d7cbd97a76f88601e87ea0cabf744c27c820d1
rsh-12 <grinvichforum12@mail.ru>
2021-11-23 19:07:11 +0500

    Add showStudentsWithoutContracts method

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Income} from "../../common/income";
 import {CommonService} from "../../services/common.service";
+import {NotificationService} from "../../services/notification.service";
+import {StudentService} from "../../services/student.service";
 
 @Component({
     selector: 'app-home',
@@ -11,8 +13,11 @@ export class HomeComponent implements OnInit {
 
     income: Income[] = [];
     loading = false;
+    panelOpenState = false;
 
-    constructor(private commonService: CommonService) {
+    constructor(private commonService: CommonService,
+                private studentService: StudentService,
+                private notificationService: NotificationService) {
     }
 
     ngOnInit(): void {
@@ -30,4 +35,17 @@ export class HomeComponent implements OnInit {
             () => this.loading = false
         );
     }
+
+    showStudentsWithoutContracts() {
+        this.studentService.findWithoutContracts().subscribe(
+            data => {
+                console.log(data);
+                this.notificationService.openDialogWithData(data);
+            }, err => {
+                console.log(err);
+                this.notificationService.openSnackBar(err.error.message);
+            }
+        );
+    }
+
 }
