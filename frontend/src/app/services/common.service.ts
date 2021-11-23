58b2526fab05d6308786bbe010cdcc8080131d30
rsh-12 <grinvichforum12@mail.ru>
2021-11-23 19:05:11 +0500

    Add some methods

--- frontend/src/app/services/common.service.ts
+++ frontend/src/app/services/common.service.ts
@@ -1,11 +1,14 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {TotalRecords} from "../common/totalRecords";
 import {CurrentUser} from "../common/currentUser";
 import {Income} from "../common/income";
 import {Contract} from "../common/contract";
 import {CertificateInfo} from "../common/certificateInfo";
+import {InstructorService} from "./instructor.service";
+import {StudentService} from "./student.service";
+import {timeout} from "rxjs/operators";
 
 const API_URL = 'http://localhost:8080/api/';
 
@@ -14,7 +17,9 @@ const API_URL = 'http://localhost:8080/api/';
 })
 export class CommonService {
 
-    constructor(private http: HttpClient) {
+    constructor(private http: HttpClient,
+                private instructorService: InstructorService,
+                private studentService: StudentService) {
     }
 
     getStatistics(): Observable<TotalRecords> {
@@ -35,6 +40,28 @@ export class CommonService {
 
     getCertificates(): Observable<CertificateInfo[]> {
         return this.http.get<CertificateInfo[]>(API_URL + 'certificates');
+    }
+
+    // load data with specific service class
+    defineLoadingMethod(isRelated: boolean, entityId: number, entityName: string) {
+        const serviceClass = this.defineServiceClass(entityName);
+
+        return isRelated
+            ? serviceClass.findByCourse(entityId)
+            : serviceClass.findExceptCourse(entityId);
+    }
+
+    // modify data with specific service class
+    defineModifyingMethod(isAdding: boolean, entityId: number, entityName: string, data: { ids: number[] }) {
+        const serviceClass = this.defineServiceClass(entityName);
+
+        return isAdding
+            ? serviceClass.addToCourse(entityId, data)
+            : serviceClass.removeFromCourse(entityId, data);
+    }
+
+    private defineServiceClass(entityName: string) {
+        return entityName === 'instructors' ? this.instructorService : this.studentService;
     }
 
 }
