fbec7dc15d29c7e27a811df891ae404b91700d02
rsh-12 <grinvichforum12@mail.ru>
2021-11-23 19:06:52 +0500

    Add some methods

--- frontend/src/app/services/student.service.ts
+++ frontend/src/app/services/student.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Student} from "../common/student";
+import {CommonData} from "../common/commonData";
 
 const API_URL = 'http://localhost:8080/api/students/';
 
@@ -15,6 +16,26 @@ export class StudentService {
 
     findAll(): Observable<Student[]> {
         return this.http.get<Student[]>(API_URL);
+    }
+
+    findWithoutContracts(): Observable<Student[]> {
+        return this.http.get<Student[]>(`${API_URL}without/contracts`);
+    }
+
+    findByCourse(id: number): Observable<CommonData[]> {
+        return this.http.get<CommonData[]>(`${API_URL}course/${id}`);
+    }
+
+    findExceptCourse(id: number) {
+        return this.http.get<CommonData[]>(`${API_URL}course/${id}?except=true`);
+    }
+
+    addToCourse(id: number, data: { ids: number[] }) {
+        return this.http.post(`${API_URL}course/${id}?add=true`, data, {responseType: 'text'});
+    }
+
+    removeFromCourse(id: number, data: { ids: number[] }) {
+        return this.http.post(`${API_URL}course/${id}`, data, {responseType: 'text'});
     }
 
 }
