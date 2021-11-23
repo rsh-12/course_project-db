91ffbbfcfe1e7f9c215ec2bbed6c672918f915e0
rsh-12 <grinvichforum12@mail.ru>
2021-11-23 19:02:32 +0500

    Add some methods
    I've added findByCourse, findExceptCourse, removeFromCourse
    and addToCourse methods

--- frontend/src/app/services/instructor.service.ts
+++ frontend/src/app/services/instructor.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Instructor} from "../common/instructor";
+import {CommonData} from "../common/commonData";
 
 const API_URL = 'http://localhost:8080/api/instructors/';
 
@@ -15,6 +16,22 @@ export class InstructorService {
 
     findAll(): Observable<Instructor[]> {
         return this.http.get<Instructor[]>(API_URL);
+    }
+
+    findByCourse(id: number): Observable<CommonData[]> {
+        return this.http.get<CommonData[]>(`${API_URL}course/${id}`);
+    }
+
+    findExceptCourse(id: number) {
+        return this.http.get<CommonData[]>(`${API_URL}course/${id}?except=true`);
+    }
+
+    removeFromCourse(id: number, data: { ids: number[] }) {
+        return this.http.post(`${API_URL}course/${id}`, data, {responseType: 'text'});
+    }
+
+    addToCourse(id: number, data: { ids: number[] }) {
+        return this.http.post(`${API_URL}course/${id}?add=true`, data, {responseType: 'text'});
     }
 
 }
