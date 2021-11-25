ecda50cd8c1819b94b7e443d5cbe0bf576264372
rsh-12 <grinvichforum12@mail.ru>
2021-11-25 10:55:03 +0500

    Update dialog-data.component.ts

--- frontend/src/app/components/dialog-data/dialog-data.component.ts
+++ frontend/src/app/components/dialog-data/dialog-data.component.ts
@@ -1,15 +1,16 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialog} from "@angular/material/dialog";
+import {MAT_DIALOG_DATA} from "@angular/material/dialog";
+import {Student} from "../../common/student";
 
 @Component({
     selector: 'app-dialog-data',
     template: `
         <div>
-            <h1 mat-dialog-title>Students without contracts</h1>
+            <h1 mat-dialog-title>{{data.title}}</h1>
             <div mat-dialog-content class="card-block">
                 <ul>
-                    <li *ngFor="let d of data">
-                        <a matDialogClose [routerLink]="['/courses', d.id]">
+                    <li *ngFor="let d of data.students">
+                        <a matDialogClose [routerLink]="['/students', d.id]">
                             {{d.lastName}} {{d.firstName}}
                         </a>
                     </li>
@@ -19,11 +20,6 @@ import {MAT_DIALOG_DATA, MatDialog} from "@angular/material/dialog";
 
     `,
     styles: [`
-        .card-block {
-            max-height: 200px;
-            overflow: auto;
-        }
-
         ul {
             list-style-type: none;
         }
@@ -35,7 +31,7 @@ import {MAT_DIALOG_DATA, MatDialog} from "@angular/material/dialog";
 })
 export class DialogDataComponent implements OnInit {
 
-    constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
+    constructor(@Inject(MAT_DIALOG_DATA) public data: { students: Student[], title: '' }) {
     }
 
     ngOnInit(): void {
