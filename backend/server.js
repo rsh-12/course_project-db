dbadd4849472afd6b03dececcace9935675af45e
rsh-12 <grinvichforum12@mail.ru>
2021-11-26 17:18:10 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,22 @@
 const app = require('./app');
+const {redisClient} = require('./src/config/redis.config');
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server is running: http://localhost:${PORT}.`);
 });
 
+const cleanup = () => {
+    redisClient.quit()
+        .then(result => {
+            if (result) console.log('Connection is closed')
+        }).catch(e => {
+        console.log(e.message);
+    });
+
+    server.close();
+};
+
+process.on('SIGINT', cleanup);
+process.on('SIGTERM', cleanup);
