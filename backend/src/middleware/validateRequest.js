d3af60d9a203d059bd15f2d3767497aa5730492f
rsh-12 <grinvichforum12@mail.ru>
2021-11-29 21:48:43 +0500

    Add studentUniquePhoneAndEmail function

--- backend/src/middleware/validateRequest.js
+++ backend/src/middleware/validateRequest.js
@@ -1,4 +1,5 @@
 const CourseRepo = require("../repository/course.repo");
+const StudentRepo = require("../repository/student.repo");
 
 allArgsProvided = (res, ...args) => {
     const allArgsProvided = args.filter(value => value !== undefined).length === args.length;
@@ -6,6 +7,16 @@ allArgsProvided = (res, ...args) => {
     if (!allArgsProvided) {
         return res.status(400).send({message: 'Not all arguments are passed'})
     }
+}
+
+studentUniquePhoneAndEmail = async (req, res, next) => {
+    const {phone, email} = req.body;
+    const exists = await StudentRepo.existsByPhoneOrEmail(phone, email);
+    if (exists) {
+        return res.status(400).send({message: 'Email address or phone number is already in use'});
+    }
+
+    next();
 }
 
 courseUniqueName = async (req, res, next) => {
@@ -19,7 +30,8 @@ courseUniqueName = async (req, res, next) => {
 
 const validateRequest = {
     allArgsProvided,
-    courseUniqueName
+    courseUniqueName,
+    studentUniquePhoneAndEmail,
 }
 
 module.exports = validateRequest;
