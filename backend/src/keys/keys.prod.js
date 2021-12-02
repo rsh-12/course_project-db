8c8995dd2266f01c5c6e1ee7b206a6071eceb6be
rsh-12 <grinvichforum12@mail.ru>
2021-12-02 12:19:18 +0500

    Add keys.prod.js

---
+++ backend/src/keys/keys.prod.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // JWT
+    SECRET_KEY: process.env.SECRET_KEY,
+
+    // DATABASE
+    DB_HOST: process.env.DB_HOST,
+    DB_PORT: process.env.DB_PORT,
+    DB_NAME: process.env.DB_NAME,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+}
