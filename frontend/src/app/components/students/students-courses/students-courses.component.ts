2a74124a9d0aac668577a54c00f9d03fa588e08c
rsh-12 <grinvichforum12@mail.ru>
2021-12-04 19:41:23 +0500

    Update students-courses.component.ts

--- frontend/src/app/components/students/students-courses/students-courses.component.ts
+++ frontend/src/app/components/students/students-courses/students-courses.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+
+export interface StudentsWithCourses {
+    // courses_students
+    id: number,
+
+    // student
+    studentId: number,
+    lastName: string,
+    firstName: string,
+
+    // course
+    courseId: number;
+    course: string;
+}
 
 @Component({
-  selector: 'app-students-courses',
-  templateUrl: './students-courses.component.html',
-  styleUrls: ['./students-courses.component.css']
+    selector: 'app-students-courses',
+    templateUrl: './students-courses.component.html',
+    styleUrls: ['./students-courses.component.css']
 })
 export class StudentsCoursesComponent implements OnInit {
 
-  constructor() { }
+    @Input() loadStudentsWithoutContracts = true;
 
-  ngOnInit(): void {
-  }
+    loading = false;
+    data: StudentsWithCourses[] = [];
 
+    constructor() {
+    }
+
+    ngOnInit(): void {
+        this.loadStudentsWithoutContracts
+            ? this.retrieveStudentsWithoutContracts()
+            : this.retrieveStudentsWithoutCertificates();
+    }
+
+    private retrieveStudentsWithoutContracts() {
+
+    }
+
+    private retrieveStudentsWithoutCertificates() {
+
+    }
 }
