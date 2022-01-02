beb041930764695cdb9b1d25ca3b46424627088d
rsh-12 <grinvichforum12@mail.ru>
2022-01-02 13:13:56 +0500

    Add commonValidators, isNumeric static methods

--- frontend/src/app/services/utils.service.ts
+++ frontend/src/app/services/utils.service.ts
@@ -1,11 +1,28 @@
 import {Injectable} from '@angular/core';
 import {HttpErrorResponse} from "@angular/common/http";
 import {Observable, throwError} from "rxjs";
+import {Validators} from "@angular/forms";
 
 @Injectable({
     providedIn: 'root'
 })
 export class UtilsService {
+
+    static nonWhitespaceRegExp: RegExp = new RegExp("^\\S");
+    router: any;
+
+    static commonValidators(min = 5, max = 50) {
+        return [
+            Validators.required,
+            Validators.pattern(this.nonWhitespaceRegExp),
+            Validators.minLength(min),
+            Validators.maxLength(max),
+        ];
+    }
+
+    static isNumeric(val: string): boolean {
+        return /^\d+$/.test(val);
+    }
 
     static handleError(error: HttpErrorResponse): Observable<never> {
         if (error.status === 0) {
