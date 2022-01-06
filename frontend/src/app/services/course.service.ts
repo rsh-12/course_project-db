0de9c50c4d243e7631a1f79d15f6e9e3f87d250a
rsh-12 <grinvichforum12@mail.ru>
2022-01-06 08:41:12 +0500

    Update course.service.ts

--- frontend/src/app/services/course.service.ts
+++ frontend/src/app/services/course.service.ts
@@ -4,6 +4,8 @@ import {Observable} from "rxjs";
 import {CourseById} from "../common/courseById";
 import {Course} from "../common/course";
 import {environment} from "../../environments/environment";
+import {catchError} from "rxjs/operators";
+import {UtilsService} from "./utils.service";
 
 const API_URL = environment.API_URL + 'courses/';
 
@@ -21,10 +23,11 @@ export class CourseService {
                 params: {
                     instructorId
                 }
-            });
+            }).pipe(catchError(UtilsService.handleError));
         }
 
-        return this.http.get<Course[]>(API_URL);
+        return this.http.get<Course[]>(API_URL)
+            .pipe(catchError(UtilsService.handleError));
     }
 
     findByName(name: string): Observable<Course[]> {
@@ -32,23 +35,27 @@ export class CourseService {
             params: {
                 name
             }
-        });
+        }).pipe(catchError(UtilsService.handleError));
     }
 
     findById(id: string): Observable<CourseById> {
-        return this.http.get<CourseById>(API_URL + id);
+        return this.http.get<CourseById>(API_URL + id)
+            .pipe(catchError(UtilsService.handleError));
     }
 
     update(id: number, data: any): Observable<any> {
-        return this.http.put(API_URL + id, data);
+        return this.http.put(API_URL + id, data)
+            .pipe(catchError(UtilsService.handleError));
     }
 
     delete(id: number): Observable<any> {
-        return this.http.delete(API_URL + id);
+        return this.http.delete(API_URL + id)
+            .pipe(catchError(UtilsService.handleError));
     }
 
     add(data: any): Observable<any> {
-        return this.http.post(API_URL, data);
+        return this.http.post(API_URL, data)
+            .pipe(catchError(UtilsService.handleError));
     }
 
 }
