7c64bf5329c5a9b3dc1df51e2ff9faf8d43d86e1
rsh-12 <grinvichforum12@mail.ru>
2022-01-08 09:54:35 +0500

    Add catchError pipe operation

--- frontend/src/app/services/student.service.ts
+++ frontend/src/app/services/student.service.ts
@@ -5,6 +5,8 @@ import {Student} from "../common/student";
 import {CommonData} from "../common/commonData";
 import {StudentsWithCourses} from "../components/students/students-courses/students-courses.component";
 import {environment} from "../../environments/environment";
+import {catchError} from "rxjs/operators";
+import {UtilsService} from "./utils.service";
 
 const API_URL = environment.API_URL + 'students/';
 
@@ -17,19 +19,23 @@ export class StudentService {
     }
 
     findAll(): Observable<Student[]> {
-        return this.http.get<Student[]>(API_URL);
+        return this.http.get<Student[]>(API_URL)
+            .pipe(catchError(UtilsService.handleError));
     }
 
     findWithoutContracts(): Observable<Student[]> {
-        return this.http.get<Student[]>(`${API_URL}without/contracts`);
+        return this.http.get<Student[]>(`${API_URL}without/contracts`)
+            .pipe(catchError(UtilsService.handleError));
     }
 
     findByCourse(id: number): Observable<CommonData[]> {
-        return this.http.get<CommonData[]>(`${API_URL}course/${id}`);
+        return this.http.get<CommonData[]>(`${API_URL}course/${id}`)
+            .pipe(catchError(UtilsService.handleError));
     }
 
     findByCompany(id: number): Observable<Student[]> {
-        return this.http.get<Student[]>(`${API_URL}company/${id}`);
+        return this.http.get<Student[]>(`${API_URL}company/${id}`)
+            .pipe(catchError(UtilsService.handleError));
     }
 
     findExceptCourse(id: number) {
@@ -37,7 +43,7 @@ export class StudentService {
             params: {
                 except: true
             }
-        });
+        }).pipe(catchError(UtilsService.handleError));
     }
 
     addToCourse(id: number, data: { ids: number[] }) {
@@ -46,11 +52,12 @@ export class StudentService {
             params: {
                 add: true
             }
-        });
+        }).pipe(catchError(UtilsService.handleError));
     }
 
     removeFromCourse(id: number, data: { ids: number[] }) {
-        return this.http.post(`${API_URL}course/${id}`, data, {responseType: 'text'});
+        return this.http.post(`${API_URL}course/${id}`, data, {responseType: 'text'})
+            .pipe(catchError(UtilsService.handleError));
     }
 
     findByName(studentName: string): Observable<Student[]> {
@@ -58,27 +65,32 @@ export class StudentService {
             params: {
                 name: studentName
             }
-        });
+        }).pipe(catchError(UtilsService.handleError));
     }
 
     delete(id: number) {
-        return this.http.delete(API_URL + id);
+        return this.http.delete(API_URL + id)
+            .pipe(catchError(UtilsService.handleError));
     }
 
     findById(id: string): Observable<Student> {
-        return this.http.get<Student>(API_URL + id);
+        return this.http.get<Student>(API_URL + id)
+            .pipe(catchError(UtilsService.handleError));
     }
 
     update(id: number, data: any): Observable<Object> {
-        return this.http.put(API_URL + id, data, {responseType: 'text'});
+        return this.http.put(API_URL + id, data, {responseType: 'text'})
+            .pipe(catchError(UtilsService.handleError));
     }
 
     add(data: any): Observable<Student> {
-        return this.http.post<Student>(API_URL, data);
+        return this.http.post<Student>(API_URL, data)
+            .pipe(catchError(UtilsService.handleError));
     }
 
     findWithCoursesWithout(data: string): Observable<StudentsWithCourses[]> {
-        return this.http.get<StudentsWithCourses[]>(API_URL + 'courses/' + data);
+        return this.http.get<StudentsWithCourses[]>(API_URL + 'courses/' + data)
+            .pipe(catchError(UtilsService.handleError));
     }
 
 }
